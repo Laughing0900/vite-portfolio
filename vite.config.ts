4a2c6d5ca17ccd1f35e0cb880b4fc9fd8a902813
laughing0900@gmail.com <laughing0900@gmail.com>
2024-06-10 11:02:59 +0800

    chore: vite use swc for development

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from "path";
 import MillionLint from "@million/lint";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
