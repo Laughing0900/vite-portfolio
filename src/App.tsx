7b035b34efa56452e70e7a8170bf663b3776e660
laughing0900@gmail.com <laughing0900@gmail.com>
2024-06-11 21:24:15 +0800

    feat: put down dev mode dialog

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,6 @@ import Certificate from "@/components/certificate/certificate";
 import Experiences from "@/components/experience/experiences";
 import MainLayout from "@/components/layout/mainLayout";
 import Project from "@/components/project/project";
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-} from "@/components/ui/dialog";
 
 function App() {
     return (
@@ -22,18 +15,6 @@ function App() {
                 <Experiences />
                 <Certificate />
             </MainLayout>
-            <Dialog defaultOpen>
-                <DialogContent>
-                    <DialogHeader>
-                        <DialogTitle>Is this ready?</DialogTitle>
-                        <DialogDescription>
-                            This portfolio is still under development. Some
-                            feature may not be stable. Keep going to beta
-                            version.
-                        </DialogDescription>
-                    </DialogHeader>
-                </DialogContent>
-            </Dialog>
         </>
     );
 }
