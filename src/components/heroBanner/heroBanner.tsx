61ce185d66ce73e245829ac0b9d91ce549ae847e
laughing0900@gmail.com <laughing0900@gmail.com>
2024-06-12 09:22:29 +0800

    Update heroBanner component and mainLayout component

--- src/components/heroBanner/heroBanner.tsx
+++ src/components/heroBanner/heroBanner.tsx
@@ -1,27 +1,14 @@
-import { motion, useTransform } from "framer-motion";
 import { FC } from "react";
 import { FlipWords } from "@/components/heroBanner/flip-words";
 import { HeroHighlight } from "@/components/heroBanner/heroBackground";
-import type { MotionValue } from "framer-motion";
-
-type HeroBannerProps = {
-    scrollYProgress: MotionValue;
-};
 
 const words = ["a Remarkable", "a Successful", "a Stunning", "an Outstanding"];
 
-const HeroBanner: FC<HeroBannerProps> = ({ scrollYProgress }) => {
-    const progress = [0, 0.1];
-    const opacity = useTransform(scrollYProgress, progress, [1, 0]);
-    const visibility = useTransform(scrollYProgress, progress, [
-        "inherit",
-        "hidden",
-    ]);
+const HeroBanner: FC = () => {
     return (
-        <motion.section
+        <section
             id="hero-banner"
             className="fixed bottom-auto left-0 right-0 top-0 z-10 h-dvh"
-            style={{ opacity, visibility }}
         >
             <HeroHighlight className="grid-template h-full w-full items-center">
                 <p className="col-span-4 pb-40 text-5xl font-normal leading-[1.25] text-foreground md:col-span-6 md:col-start-2 md:text-6xl">
@@ -39,7 +26,7 @@ const HeroBanner: FC<HeroBannerProps> = ({ scrollYProgress }) => {
                     Laughing
                 </div>
             </HeroHighlight>
-        </motion.section>
+        </section>
     );
 };
 export default HeroBanner;
--- src/components/layout/mainLayout.tsx
+++ src/components/layout/mainLayout.tsx
@@ -8,7 +8,7 @@ import GridBackground from "@/components/ui/background-grids";
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
 import useBreakpoint from "@/hooks/useBreakpoint";
-import type { FC, PropsWithChildren, ReactNode } from "react";
+import type { FC, PropsWithChildren } from "react";
 
 const MainLayout: FC<PropsWithChildren> = ({ children }) => {
     const { isMobile } = useBreakpoint();
@@ -18,12 +18,17 @@ const MainLayout: FC<PropsWithChildren> = ({ children }) => {
         offset: ["start end", "end end"],
     });
     const opacity = useTransform(scrollYProgress, [0.7, 1], [0.8, 0]);
+    const progress = [0, 0.1];
+    const heroOpacity = useTransform(scrollYProgress, progress, [1, 0]);
+    const visibility = useTransform(scrollYProgress, progress, [
+        "inherit",
+        "hidden",
+    ]);
 
     const [openGrid, setOpenGrid] = useState(false);
-
     const toggleGrid = useCallback(() => {
         setOpenGrid((prev) => !prev);
-    }, []);
+    }, [setOpenGrid]);
 
     const Background = useMemo(() => {
         if (isMobile) {
@@ -45,8 +50,9 @@ const MainLayout: FC<PropsWithChildren> = ({ children }) => {
                     />
                 </div>
             </header>
-
-            <HeroBanner scrollYProgress={scrollYProgress} />
+            <motion.div style={{ opacity: heroOpacity, visibility }}>
+                <HeroBanner />
+            </motion.div>
             <main className="relative z-10 mt-[100dvh] min-h-dvh w-full bg-background">
                 {Background}
                 <div className="-mt-[100dvh] space-y-40 pt-40">{children}</div>
