c3c0ef666fc430a093f4eb6334f06191c6b238d0
laughing0900@gmail.com <laughing0900@gmail.com>
2024-06-12 13:32:07 +0800

    feat: store dialog read to local

--- src/components/ui/developmentMode.tsx
+++ src/components/ui/developmentMode.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { useLocalStorage } from "react-use";
 import {
     Dialog,
     DialogContent,
@@ -7,16 +8,25 @@ import {
     DialogTitle,
 } from "@/components/ui/dialog";
 
-export const WithDevAlert: FC = () => (
-    <Dialog defaultOpen>
-        <DialogContent>
-            <DialogHeader>
-                <DialogTitle>Is this ready?</DialogTitle>
-                <DialogDescription>
-                    This portfolio is still under development. Some feature may
-                    not stable. Keep going to beta version.
-                </DialogDescription>
-            </DialogHeader>
-        </DialogContent>
-    </Dialog>
-);
+export const WithDevAlert: FC = () => {
+    const [accept, setAccept] = useLocalStorage("devMode", false);
+
+    return (
+        <Dialog
+            defaultOpen={!accept}
+            onOpenChange={() => {
+                setAccept(true);
+            }}
+        >
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Is this ready?</DialogTitle>
+                    <DialogDescription>
+                        This portfolio is still under development. Some feature
+                        may not stable. Keep going to beta version.
+                    </DialogDescription>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+    );
+};
