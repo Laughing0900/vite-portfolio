274d0a03f41e5bd7ca85248895fe2192c6417599
laughing0900@gmail.com <laughing0900@gmail.com>
2024-06-20 15:01:48 +0800

    stash

--- src/components/layout/loading.tsx
+++ src/components/layout/loading.tsx
@@ -1,10 +1,22 @@
-const Loading = () => {
-    const getScreenFontSize = () => {};
-    return (
-        <div className="fixed left-0 top-0 h-screen w-full">
-            <div className="">LOAD...ING</div>
-            <div className="">LAUGHING</div>
+import { useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
+const Loading = () => {
+    const ref = useRef<HTMLDivElement>(null);
+
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ["start end", "end end"],
+    });
+
+    const scale = useTransform(scrollYProgress, [0.7, 1], [0.8, 0]);
+
+    return (
+        <div className="fixed left-0 top-0 h-screen w-full" ref={ref}>
+            <div className="text-[calc(100vw/6)]">
+                <div className="">LOAD...ING</div>
+                <div className="">LAUGHING</div>
+            </div>
             <div className="">READY</div>
         </div>
     );
