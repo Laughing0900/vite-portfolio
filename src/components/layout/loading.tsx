597cff35c3fe00a94de991f141d99009c6ce4c81
laughing0900@gmail.com <laughing0900@gmail.com>
2024-06-25 15:08:28 +0800

    feat: update loading page

--- src/components/layout/loading.tsx
+++ src/components/layout/loading.tsx
@@ -1,12 +1,54 @@
-const Loading = () => {
-    const getScreenFontSize = () => {};
-    return (
-        <div className="fixed left-0 top-0 h-screen w-full">
-            <div className="">LOAD...ING</div>
-            <div className="">LAUGHING</div>
+import {
+    motion,
+    useMotionValueEvent,
+    useScroll,
+    useTransform,
+} from "framer-motion";
+import { Scale } from "lucide-react";
+import { useRef, useState } from "react";
+import useOnFetching from "@/hooks/useOnFetching";
 
-            <div className="">READY</div>
-        </div>
+const Loading = () => {
+    const { isLoading } = useOnFetching();
+    const [scrollOut, setScrollOut] = useState(true);
+    const ref = useRef<HTMLDivElement>(null);
+
+    const variants = {
+        show: {
+            scale: 1,
+            transition: {
+                ease: "easeOut",
+                duration: 0.3,
+            },
+        },
+        hide: {
+            scale: 0,
+        },
+    };
+
+    return (
+        <>
+            {scrollOut && (
+                <motion.div
+                    className="fixed left-1/2 top-0 h-[200dvh] w-full -translate-x-1/2 transform bg-white"
+                    ref={ref}
+                >
+                    <div className="glitch-loading font-goldman text-black">
+                        Load...ing
+                    </div>
+                    {!isLoading && (
+                        <div
+                            className="bottom-[10%] left-1/2 -translate-x-1/2 transform"
+                            onClick={() => {
+                                setScrollOut(false);
+                            }}
+                        >
+                            Enter
+                        </div>
+                    )}
+                </motion.div>
+            )}
+        </>
     );
 };
 
