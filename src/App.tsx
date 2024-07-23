5b724ff882fe8c2c0d2117c60dab12a43f56c239
laughing0900@gmail.com <laughing0900@gmail.com>
2024-07-23 08:43:35 +0800

    rm: unused  loading feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useEffect } from "react";
-import { useLockBodyScroll, useToggle } from "react-use";
 import About from "@/components/about/about";
 import Certificate from "@/components/certificate/certificate";
 import Experiences from "@/components/experience/experiences";
-import Loading from "@/components/layout/loading";
 import MainLayout from "@/components/layout/mainLayout";
 import Project from "@/components/project/project";
-import useOnFetching from "@/hooks/useOnFetching";
 
 function App() {
-    const { isLoading } = useOnFetching();
-
-    const [locked, toggleLocked] = useToggle(false);
-    useLockBodyScroll(locked);
-
-    useEffect(() => {
-        toggleLocked(isLoading);
-    }, [isLoading, toggleLocked]);
-
     return (
-        <>
-            <Loading />
-            {!isLoading && (
-                <MainLayout>
-                    <About />
-                    <Project />
-                    <Experiences />
-                    <Certificate />
-                </MainLayout>
-            )}
-        </>
+        <MainLayout>
+            <About />
+            <Project />
+            <Experiences />
+            <Certificate />
+        </MainLayout>
     );
 }
 
