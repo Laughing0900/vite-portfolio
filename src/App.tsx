3d66d513617122f9e07fa8ca6d406e8666d6b4f0
laughing0900@gmail.com <laughing0900@gmail.com>
2024-07-23 09:25:58 +0800

    feat: Add navigation component to main layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import About from "@/components/about/about";
 import Certificate from "@/components/certificate/certificate";
 import Experiences from "@/components/experience/experiences";
 import MainLayout from "@/components/layout/mainLayout";
+import WithNav from "@/components/layout/nav";
 import Project from "@/components/project/project";
 
 function App() {
     return (
         <MainLayout>
             <About />
+            <WithNav />
             <Project />
             <Experiences />
             <Certificate />
---
+++ src/components/layout/nav.tsx
@@ -0,0 +1,80 @@
+import {
+    AppWindowMac,
+    Building2,
+    GraduationCap,
+    HomeIcon,
+    Send,
+} from "lucide-react";
+import { buttonVariants } from "@/components/ui/button";
+import { Dock, DockIcon } from "@/components/ui/dock";
+import Link from "@/components/ui/link";
+import { Separator } from "@/components/ui/separator";
+import { cn } from "@/lib/utils";
+
+const DATA = {
+    contact: {
+        project: {
+            name: "Projects",
+            url: "#",
+            icon: AppWindowMac,
+        },
+        Experience: {
+            name: "Experiences",
+            url: "#",
+            icon: Building2,
+        },
+        cert: {
+            name: "Cert. & Edu.",
+            url: "#",
+            icon: GraduationCap,
+        },
+    },
+    navbar: [
+        { href: "#", icon: Send, label: "Contact me" },
+        { href: "#", icon: HomeIcon, label: "Home" },
+    ],
+};
+
+const WithNav = () => {
+    return (
+        <div className="fixed bottom-5 left-0 z-50 w-full">
+            <Dock direction="middle">
+                {Object.entries(DATA.contact).map(([name, contact]) => (
+                    <DockIcon key={name}>
+                        <Link
+                            href={contact.url}
+                            className={cn(
+                                buttonVariants({
+                                    variant: "ghost",
+                                    size: "icon",
+                                }),
+                                "size-12 rounded-full"
+                            )}
+                        >
+                            <contact.icon className="size-4" />
+                        </Link>
+                    </DockIcon>
+                ))}
+                <Separator orientation="vertical" className="h-full" />
+                {DATA.navbar.map((item) => (
+                    <DockIcon key={item.label}>
+                        <Link
+                            href={item.href}
+                            className={cn(
+                                buttonVariants({
+                                    variant: "ghost",
+                                    size: "icon",
+                                }),
+                                "size-12 rounded-full"
+                            )}
+                        >
+                            <item.icon className="size-4" />
+                        </Link>
+                    </DockIcon>
+                ))}
+            </Dock>
+        </div>
+    );
+};
+
+export default WithNav;
