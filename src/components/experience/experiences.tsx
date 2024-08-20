3c7ef5964d7ed53de51de28d8a7860d4c3654c02
laughing0900@gmail.com <laughing0900@gmail.com>
2024-08-20 17:34:26 +0800

    feat: move scroll bar to new component

--- src/components/experience/experiences.tsx
+++ src/components/experience/experiences.tsx
@@ -1,6 +1,7 @@
 import { motion, useMotionValueEvent, useScroll } from "framer-motion";
 import { useMemo, useRef, useState } from "react";
 import ExperienceCard from "@/components/experience/contentCard/experienceCard";
+import ExperiencesScrollBar from "@/components/experience/experiencesScrollBar";
 import { useExperiences } from "@/components/experience/hooks/useExperiences";
 import RoleCard from "@/components/experience/roleCard";
 import useBreakpoint from "@/hooks/useBreakpoint";
@@ -73,52 +74,7 @@ const Experiences = () => {
                             })}
                         </div>
                         {/* @scroll bar */}
-                        <div
-                            className="absolute right-0 top-40 hidden w-1 flex-col gap-4 text-[88px] lg:flex"
-                            style={{
-                                height: `${filedCompanies.length}em`,
-                            }}
-                        >
-                            {filedCompanies.map((_, index) => {
-                                return (
-                                    <motion.div
-                                        key={"exp_bar_" + index}
-                                        className="relative w-full rounded-full bg-gray-700/50"
-                                        style={{
-                                            height: `calc(100% / ${filedCompanies.length})`,
-                                        }}
-                                        animate={{
-                                            transition: {
-                                                duration: 0.15,
-                                            },
-                                        }}
-                                    >
-                                        {selected === index && (
-                                            <motion.span
-                                                className="absolute inset-0 block h-full w-full rounded-full lg:bg-white"
-                                                layoutId="hoverBackground"
-                                                initial={{
-                                                    opacity: 0,
-                                                }}
-                                                animate={{
-                                                    opacity: 1,
-                                                    transition: {
-                                                        duration: 0.15,
-                                                    },
-                                                }}
-                                                exit={{
-                                                    opacity: 0,
-                                                    transition: {
-                                                        duration: 0.15,
-                                                    },
-                                                }}
-                                                key={"desktop_exp_bar" + index}
-                                            />
-                                        )}
-                                    </motion.div>
-                                );
-                            })}
-                        </div>
+                        <ExperiencesScrollBar selected={selected} />
                     </div>
                 )}
                 <div
---
+++ src/components/experience/experiencesScrollBar.tsx
@@ -0,0 +1,68 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { useMemo } from "react";
+import { useExperiences } from "@/components/experience/hooks/useExperiences";
+
+type ExperiencesScrollBarProps = {
+    selected: number;
+};
+
+const ExperiencesScrollBar = ({ selected }: ExperiencesScrollBarProps) => {
+    const { companies } = useExperiences();
+    const { filedCompanies } = useMemo(() => {
+        return {
+            filedCompanies: companies.filter((company) => company.description),
+        };
+    }, [companies]);
+    return (
+        <div
+            className="absolute right-0 top-40 hidden w-1 flex-col gap-4 text-[88px] lg:flex"
+            style={{
+                height: `${filedCompanies.length}em`,
+            }}
+        >
+            {filedCompanies.map((_, index) => {
+                return (
+                    <motion.div
+                        key={"exp_bar_" + index}
+                        className="relative w-full rounded-full bg-gray-700/50"
+                        style={{
+                            height: `calc(100% / ${filedCompanies.length})`,
+                        }}
+                        animate={{
+                            transition: {
+                                duration: 0.15,
+                            },
+                        }}
+                    >
+                        <AnimatePresence>
+                            {selected === index && (
+                                <motion.span
+                                    className="absolute inset-0 block h-full w-full rounded-full lg:bg-white"
+                                    layoutId="hoverBackground"
+                                    initial={{
+                                        opacity: 0,
+                                    }}
+                                    animate={{
+                                        opacity: 1,
+                                        transition: {
+                                            duration: 0.15,
+                                        },
+                                    }}
+                                    exit={{
+                                        opacity: 0,
+                                        transition: {
+                                            duration: 0.15,
+                                        },
+                                    }}
+                                    key={"desktop_exp_bar" + index}
+                                />
+                            )}
+                        </AnimatePresence>
+                    </motion.div>
+                );
+            })}
+        </div>
+    );
+};
+
+export default ExperiencesScrollBar;
