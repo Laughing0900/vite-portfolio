5c448646f72e33fc3833a8c250ac75bf07f3e2af
laughing0900@gmail.com <laughing0900@gmail.com>
2024-10-01 12:35:13 +0800

    feat(layout): replace background with gradient animation background

--- src/components/layout/mainLayout.tsx
+++ src/components/layout/mainLayout.tsx
@@ -5,17 +5,10 @@ import FooterOverlay from "@/components/layout/footer/footerOverlay";
 import WithGrid from "@/components/layout/grid";
 import WithHeader from "@/components/layout/header";
 import WithNav from "@/components/layout/nav";
-import { BoxesBackground } from "@/components/ui/background-boxes";
-import GridBackground from "@/components/ui/background-grids";
-import useBreakpoint from "@/hooks/useBreakpoint";
-import useDevice from "@/hooks/useDevice";
+import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
 import type { FC, PropsWithChildren } from "react";
 
 const MainLayout: FC<PropsWithChildren> = ({ children }) => {
-    const { isMobile: md } = useBreakpoint();
-    const { isDesktop } = useDevice();
-    const isMobile = !isDesktop || md;
-
     const [openGrid, setOpenGrid] = useState(false);
 
     return (
@@ -27,7 +20,7 @@ const MainLayout: FC<PropsWithChildren> = ({ children }) => {
             <HeroBanner />
 
             <main className="relative z-30 mt-[100dvh] min-h-dvh w-full bg-background">
-                {isMobile ? <GridBackground /> : <BoxesBackground />}
+                <BackgroundGradientAnimation />
                 <div className="-mt-[100dvh] space-y-40 pt-40">{children}</div>
             </main>
 
