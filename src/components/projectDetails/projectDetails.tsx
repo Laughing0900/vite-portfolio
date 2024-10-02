799d77db8f8b21e84360c5468587053d5ba023ba
laughing0900@gmail.com <laughing0900@gmail.com>
2024-10-02 17:21:45 +0800

    feat(projectDetails): add tooltips and image display

--- src/components/projectDetails/projectDetails.tsx
+++ src/components/projectDetails/projectDetails.tsx
@@ -2,7 +2,14 @@ import { CornerDownLeft, SquareArrowOutUpRight } from "lucide-react";
 import { Link as RouterLink } from "react-router-dom";
 import MainLayout from "@/components/layout/mainLayout";
 import { useProjectsDetails } from "@/components/projectDetails/hooks/useProjectsDetails";
+import Image from "@/components/ui/image";
 import Link from "@/components/ui/link";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 const ProjectDetails: React.FC<{ id: string }> = ({ id }) => {
     const { data: response, isLoading } = useProjectsDetails({ projectId: id });
@@ -22,29 +29,55 @@ const ProjectDetails: React.FC<{ id: string }> = ({ id }) => {
                             :- {company}
                         </p>
                     </div>
-                    <div className="mt-4 grid grid-cols-3 gap-5">
-                        <div className="rounded-8 aspect-square border-2 border-gray-500 p-10 hover:bg-gray-500/50">
-                            <RouterLink to="/vite-portfolio">
-                                <CornerDownLeft className="h-full w-full" />
-                            </RouterLink>
+                    <TooltipProvider>
+                        <div className="mt-4 grid grid-cols-3 gap-5">
+                            <div className="rounded-8 aspect-square border-2 border-gray-500 p-10 hover:bg-gray-500/50">
+                                <RouterLink to="/vite-portfolio">
+                                    <Tooltip delayDuration={100}>
+                                        <TooltipTrigger asChild>
+                                            <CornerDownLeft className="h-full w-full" />
+                                        </TooltipTrigger>
+                                        <TooltipContent sticky="always">
+                                            <p>Back to home</p>
+                                        </TooltipContent>
+                                    </Tooltip>
+                                </RouterLink>
+                            </div>
+
+                            <Link
+                                href={href}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="rounded-8 re col-start-3 block aspect-square border-2 border-gray-500 p-10 hover:bg-gray-500/50"
+                            >
+                                <Tooltip delayDuration={100}>
+                                    <TooltipTrigger asChild>
+                                        <SquareArrowOutUpRight className="h-full w-full" />
+                                    </TooltipTrigger>
+
+                                    <TooltipContent sticky="always">
+                                        <p>View</p>
+                                    </TooltipContent>
+                                </Tooltip>
+                            </Link>
                         </div>
-                        <Link
-                            href={href}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="rounded-8 col-start-3 block aspect-square border-2 border-gray-500 p-10 hover:bg-gray-500/50"
-                        >
-                            <SquareArrowOutUpRight className="h-full w-full" />
-                        </Link>
-                    </div>
+                    </TooltipProvider>
                 </div>
 
                 {/* right */}
-                <div className="rounded-8 col-span-full h-fit border-2 border-gray-500 p-10 lg:col-span-5">
+                <div className="rounded-8 col-span-full h-fit overflow-clip border-2 border-gray-500 lg:col-span-5">
+                    <Image
+                        src={`https://res.cloudinary.com/dicmdiiov/image/upload/f_auto,q_auto/v1/Pawn/portfolio/project/${imageId}`}
+                        width="100%"
+                        className="object-cove mb-10 h-full w-full"
+                        alt="project image"
+                    />
+
                     <ul
                         dangerouslySetInnerHTML={{
                             __html: content || "",
                         }}
+                        className="p-10 drop-shadow"
                     >
                         {}
                     </ul>
