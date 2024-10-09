df5dd62ea2bac4b9a7294614a4657ead5c3d968c
laughing0900@gmail.com <laughing0900@gmail.com>
2024-10-09 11:38:49 +0800

    init: next

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Laughing's Portfolio",
+    description:
+        "Laughing's showcasing innovative web projects. Explore my portfolio for creative solutions, user-friendly interfaces, and cutting-edge web technologies. Available for exciting job opportunities.",
+    keywords:
+        "Frontend Developer, Web Development, React, Animation, Responsive Design, Portfolio, BlockChain, Web3",
+};
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="en">
+            <body>
+                <div id="root">{children}</div>
+            </body>
+        </html>
+    );
+}
