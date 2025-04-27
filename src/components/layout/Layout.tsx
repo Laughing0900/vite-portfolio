9094a99a3e04fc59dd4efe7e27c46c5cbd238b54
laughing <laughing0900@gmail.com>
2025-04-27 12:56:54 +0800

    feat(layout): add navigation link to BrandIcon in layout component

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,7 +3,7 @@ import BackgroundGraphic from "@/components/layout/BackgroundGraphic";
 import Footer from "@/components/layout/Footer";
 import Nav from "@/components/layout/nav";
 import { DotPattern } from "@/components/ui/magicui/dot-pattern";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
@@ -14,7 +14,9 @@ const Layout = () => {
       <Outlet />
       <div className="pointer-events-none fixed top-0 min-h-dvh w-full">
         <BackgroundGraphic />
-        <BrandIcon className="pt-5 pl-5" />
+        <Link to="/" className="pointer-events-auto">
+          <BrandIcon className="pt-5 pl-5" />
+        </Link>
       </div>
       <Nav />
       <Footer />
