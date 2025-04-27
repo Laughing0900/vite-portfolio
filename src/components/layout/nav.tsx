4c285bdfe0acce1684a92a9918476c86ceb2b61d
laughing <laughing0900@gmail.com>
2025-04-27 12:42:14 +0800

    feat(nav): enhance menu animations with AnimatePresence and motion effects

--- src/components/layout/nav.tsx
+++ src/components/layout/nav.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import { motion } from "motion/react";
+import { AnimatePresence, motion } from "motion/react";
 import { useCallback, useState } from "react";
 import { Link } from "react-router-dom";
 
@@ -43,25 +43,41 @@ const Nav = () => {
       >
         {isOpen ? "Close" : "Menu"}
       </Button>
-      {isOpen && (
-        <aside
-          className={cn(
-            "relative flex h-full w-full flex-col items-center justify-center gap-10",
-            "text-4xl",
-          )}
-        >
-          <motion.div className="-z-10 absolute size-full origin-top-right animate-in bg-background" />
-          {route.map(({ label, href }) => (
-            <Button
+      <aside
+        className={cn(
+          "relative flex h-full w-full flex-col items-center justify-center gap-10",
+          "text-4xl",
+          "delay-75 duration-150",
+        )}
+      >
+        {isOpen &&
+          route.map(({ label, href }, index) => (
+            <motion.div
               key={label}
-              variant={"link"}
-              className="text-4xl"
-              onClick={handleClick}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ delay: index * 0.1 + 0.1 }}
             >
-              <Link to={href}>{label}</Link>
-            </Button>
+              <Button
+                variant={"link"}
+                className="text-4xl"
+                onClick={handleClick}
+              >
+                <Link to={href}>{label}</Link>
+              </Button>
+            </motion.div>
           ))}
-        </aside>
+      </aside>
+      {isOpen && (
+        <AnimatePresence>
+          <motion.div
+            className="-z-10 absolute inset-0 origin-top-right bg-background duration-150 ease-out"
+            initial={{ scaleY: 0.2 }}
+            animate={{ scaleY: 1 }}
+            exit={{ scaleY: 0.2 }}
+          />
+        </AnimatePresence>
       )}
     </div>
   );
